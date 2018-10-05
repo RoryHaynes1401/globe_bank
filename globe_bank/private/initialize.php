8b707ade8156851e61885558ade8ac7785ac954f
R R Haynes <roryhaynes@gmail.com>
2018-10-06 01:47:44 +0900

    Add log in and log out pages

--- globe_bank/private/initialize.php
+++ globe_bank/private/initialize.php
@@ -1,5 +1,7 @@
 <?php
 ob_start(); // output buddering turned on
+
+session_start(); //turn on sessions super global
 // Assign file paths to PHP constants
 // __FILE__ returns the current path to this file
 // dirname() returns the path to the parent directory
